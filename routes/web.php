ee5f6a9460c08107f785219a0d2a7f2886c054c2
Cipto Ardiantoro <ardian@MacBook-Air-Cipto.local>
2025-05-16 20:34:13 +0700

    Memperbaiki fitur menu lebih responsif lagi

--- routes/web.php
+++ routes/web.php
@@ -3,18 +3,19 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('home');
+    return view('home', ['title' => 'Home Page']);
 });
  
 
 Route::get('/about', function () {
-    return view('about', ['nama' => 'Ardian']);
+    return view('about', ['name' => 'Ardian', 'title' => 'About']);
 });
 
+
 Route::get('/blog', function () {
-    return view('blog');
+    return view('blog', ['title' => 'Blog']);
 });
 
 Route::get('/contact', function () {
-    return view('contact');
+    return view('contact', ['title' => 'Contact']);
 });
